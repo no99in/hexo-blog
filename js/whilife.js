de1125029a25191d5a4febaf3500d6700676931a
no99in <github@no99.in>
2019-02-01 23:14:21 +0800

    Site updated: 2019-02-01 23:14:20

--- js/whilife.js
+++ js/whilife.js
@@ -1,6 +1,7 @@
-hljs.configure({useBR: true})
+
 document.addEventListener('DOMContentLoaded', (event) => {
     document.querySelectorAll('pre span').forEach((block) => {
         hljs.highlightBlock(block)
     })
 })
+
